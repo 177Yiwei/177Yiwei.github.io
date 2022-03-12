ab2ceda2259705afd6b4829b1cfb99325825a5e6
177Yiwei <50775800+177Yiwei@users.noreply.github.com>
2022-03-12 13:32:46 +0800

    Update thumdnail.js

--- thumdnail.js
+++ thumdnail.js
@@ -1,1 +1,9 @@
-
+let targetImage = document.querySelector(#'smart-image');
+targetImage.addEventListener('click', function() {
+  if (targetImage.classList.contain('small')) {
+    targetImage.classList.remove('small');
+  }
+  else {
+    targetImage.classList.add('small')
+  }
+})
