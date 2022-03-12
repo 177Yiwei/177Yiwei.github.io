8a3cc856c13fe474e3ce9651ddf9e0f8619d102e
177Yiwei <50775800+177Yiwei@users.noreply.github.com>
2022-03-12 17:40:39 +0800

    Update thumdnail.js

--- thumdnail.js
+++ thumdnail.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-   let targetImage = document.querySelector("#smart-image");
-   targetImage.addEventListener("click", function() {
+let targetImage = document.querySelector("#smart-image");
+targetImage.addEventListener("click", function() {
     alert("点击了图片");
-});
 });
